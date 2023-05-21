94dd4ec3737173fb2ed0138c8a5707f6c1c153e8
raffaddtg@gmail.com <raffaddtg@gmail.com>
2023-05-21 16:34:59 -0300

    refactoring, and fold/unfold icon made by css

--- js/index.js
+++ js/index.js
@@ -17,29 +17,39 @@ function setupInterface() {
     const activitiesContainer = document.querySelector('#activities-container')
     const themeButton = document.querySelector('#theme-btn')
 
-    actionbarInp.addEventListener('input', () => {
+    const handleSearch = () => {
         for (let i = 0; i < activitiesContainer.children.length; i++) {
             const child = activitiesContainer.children[i]
-            if (child.getTitle().toLowerCase().indexOf(actionbarInp.value.toLowerCase()) >= 0) {
+            const matchWithSearch = child.getTitle().toLowerCase().indexOf(actionbarInp.value.toLowerCase()) >= 0
+
+            if (matchWithSearch) {
                 child.classList.remove('hidden')
             } else {
                 child.classList.add('hidden')
             }
         }
-    })
+    }
+    
+    actionbarInp.addEventListener('input', handleSearch)
 
     newBtn.onclick = () => {
-        if (actionbarInp.value.trim().length == 0) return
+        var name = actionbarInp.value.trim()
 
-        const activity = {
-            title: actionbarInp.value,
+        if (name.length == 0) {
+            name = "Unnamed activity"
+        }
+
+        const activityData = {
+            title: name,
             registers: []
         }
-        data.activities.push(activity)
-        const activityElement = createActivity(activity)
+
+        data.activities.push(activityData)
+        const activityElement = createActivityNode(activityData)
         activitiesContainer.insertBefore(activityElement, activitiesContainer.firstChild)
 
         actionbarInp.value = ""
+        handleSearch()
     }
 
     exportBtn.onclick = () => {
@@ -51,102 +61,107 @@ function setupInterface() {
         applyPallete()
     }
 
-    for (let i = data.activities.length - 1; i >= 0; i--) {
-        activitiesContainer.appendChild(createActivity(data.activities[i]))
-    }
+    data.activities.forEach((activityData) => {
+        const activityNode = createActivityNode(activityData)
+        activitiesContainer.insertBefore(activityNode, activitiesContainer.firstChild)
+    })
 }
 
-function createActivity(activityData) {
-    const activity = document.createElement('div')
-    activity.classList.add('activity')
-    const div0 = document.createElement('div')
-    activity.appendChild(div0);
-    const activityTitle = document.createElement('h1')
-    activityTitle.classList.add("activity-title")
-    activityTitle.innerText = activityData.title
-    div0.appendChild(activityTitle)
-    const activityMenu = document.createElement('span')
-    activityMenu.classList.add("menu")
-    activityMenu.classList.add("activity-menu")
-    activityMenu.appendChild(createActivityMenu(activity, activityData))
-    div0.appendChild(activityMenu)
-    const div1 = document.createElement('div')
-    activity.appendChild(div1)
-    const timeSpent = document.createElement('p')
-    div1.appendChild(timeSpent)
-    const startActivity = document.createElement('button')
-    startActivity.innerText = "Start activity"
-    startActivity.classList.add('activity-start-btn')
-    div1.appendChild(startActivity)
-    const activityFoldRegisters = document.createElement('input')
-    activityFoldRegisters.classList.add('activity-fold-registers')
-    activityFoldRegisters.type = 'checkbox'
-    activityFoldRegisters.id = 'fold-'+activityData.title+'-'+parseInt(Math.random()*9999)
-    activity.append(activityFoldRegisters)
-    const activityFoldRegistersLabel = document.createElement('label')
-    activityFoldRegistersLabel.htmlFor = activityFoldRegisters.id
-    activity.append(activityFoldRegistersLabel)
-    const activityRegisters = document.createElement('ul')
-    activityRegisters.classList.add('activity-registers')
-    activity.appendChild(activityRegisters)
+function createActivityNode(activityData) {
+    const activityNode = document.createElement('div')
+    activityNode.classList.add('activity')
 
-    const addRegister = (r) => {
-        if (activityRegisters.children.length > 0) {
-            activityRegisters.insertBefore(r, activityRegisters.firstChild)
+    const topContainer = document.createElement('div')
+    activityNode.appendChild(topContainer);
+
+    const title = document.createElement('h1')
+    title.classList.add("activity-title")
+    title.innerText = activityData.title
+    topContainer.appendChild(title)
+
+    const menu = document.createElement('span')
+    menu.classList.add("menu")
+    menu.classList.add("activity-menu")
+    menu.appendChild(createActivityMenuNode(activityNode, activityData))
+    topContainer.appendChild(menu)
+
+    const bottomContainer = document.createElement('div')
+    activityNode.appendChild(bottomContainer)
+
+    const timeSpent = document.createElement('p') //Total time spent on the actvity
+    bottomContainer.appendChild(timeSpent)
+
+    const startBtn = document.createElement('button')
+    startBtn.innerText = "Start activity"
+    startBtn.classList.add('activity-start-btn')
+    bottomContainer.appendChild(startBtn)
+
+    const fold = document.createElement('input')
+    fold.classList.add('activity-fold-registers')
+    fold.type = 'checkbox'
+    fold.id = randomId()
+    activityNode.append(fold)
+
+    const foldLabel = document.createElement('label')
+    foldLabel.htmlFor = fold.id
+    activityNode.append(foldLabel)
+
+    const registerList = document.createElement('ul')
+    registerList.classList.add('activity-registers')
+    activityNode.appendChild(registerList)
+
+    activityNode.getTitle = () => {
+        return activityData.title
+    }
+
+    const addRegister = (registerNode) => {
+        if (registerList.children.length > 0) {
+            registerList.insertBefore(registerNode, registerList.firstChild)
         } else {
-            activityRegisters.appendChild(r)
+            registerList.appendChild(registerNode)
         }
     }
 
-    for (let i in activityData.registers) {
-        const r = createRegister(activityData.registers, activityData.registers[i])
-        addRegister(r)
-    }
+    activityData.registers.forEach((registerData) => {
+        const registerNode = createRegisterNode(activityData.registers, registerData)
+        addRegister(registerNode)
+    })
 
-    const hasActivedRegisters = () => activityData.registers.filter(re => re.end == null).length
+    const hasActivedRegisters = () => activityData.registers.filter(registerData => registerData.end == null).length > 0
 
     updateHandlers.push(() => {
         if (hasActivedRegisters()) {
-            startActivity.classList.add('disabled')
-        } else startActivity.classList.remove('disabled')
+            startBtn.classList.add('disabled')
+        } else startBtn.classList.remove('disabled')
 
         var time = 0
-        for (let i in activityData.registers) {
-            const e = activityData.registers[i]
-            if (e.end != null) {
-                time += e.end - e.start
-            } else {
-                time += Date.now() - e.start
-            }
-        }
+        activityData.registers.forEach((registerData) => {
+            time += registersDuration(registerData)
+        })
 
         timeSpent.innerText = "Total time spent: " + fullTimeFormat(time)
 
         return false
     })
 
-    startActivity.onclick = () => {
+    startBtn.onclick = () => {
         if (hasActivedRegisters()) return
 
-        const register = {
+        const registerData = {
             start: Date.now(),
             end: null
         }
-        activityData.registers.push(register)
+        activityData.registers.push(registerData)
 
 
-        const r = createRegister(activityData.registers, register)
-        addRegister(r)
-    }
-    
-    activity.getTitle = () => {
-        return activityData.title
+        const registerNode = createRegisterNode(activityData.registers, registerData)
+        addRegister(registerNode)
     }
 
-    return activity
+    return activityNode
 }
 
-function createActivityMenu(activity, activityData) {
+function createActivityMenuNode(activityNode, activityData) {
     const menuDiv = document.createElement('div')
 
     const option = (label, action) => {
@@ -157,7 +172,7 @@ function createActivityMenu(activity, activityData) {
     }
 
     option('Rename', (event, btn) => {
-        const title = activity.querySelector(".activity-title")
+        const title = activityNode.querySelector(".activity-title")
         const titleInput = document.createElement('input')
         titleInput.classList.add('activity-title')
         titleInput.value = title.innerText
@@ -171,7 +186,7 @@ function createActivityMenu(activity, activityData) {
                 activityData.title = titleInput.value
                 title.innerText = titleInput.value
             }
-            
+
             titleInput.replaceWith(title)
         })
     })
@@ -184,19 +199,19 @@ function createActivityMenu(activity, activityData) {
         p.classList.add('activity-delete-warning')
         div.appendChild(p)
         cancel.appendChild(div)
-        activity.replaceWith(cancel)
+        activityNode.replaceWith(cancel)
 
         var cancelled = false
         cancel.onclick = () => {
-            cancel.replaceWith(activity)
+            cancel.replaceWith(activityNode)
             cancelled = true
         }
 
-        const start = Date.now()
+        const startTimestamp = Date.now()
         updateHandlers.push(() => {
-            const remainingTime = parseInt((Date.now() - start) / 1000)
+            const remainingTime = parseInt((Date.now() - startTimestamp) / 1000)
 
-            p.innerText = 'Deleting in ' + (3 - remainingTime) + 's\nClick here to cancel'
+            p.innerHTML = 'Deleting in ' + (3 - remainingTime) + 's<br><strong>Click here to cancel</strong>'
 
             if (cancelled) return true
 
@@ -211,68 +226,92 @@ function createActivityMenu(activity, activityData) {
     return menuDiv
 }
 
-function createRegister(registers, register) {
-    const listItem = document.createElement('li')
-    const activityRegisterTime = document.createElement('span')
-    activityRegisterTime.classList.add('activity-register-time')
-    activityRegisterTime.innerText = '00:53'
-    listItem.appendChild(activityRegisterTime)
-    const activityRegisterDate = document.createElement('span')
-    activityRegisterDate.classList.add('activity-register-date')
-    activityRegisterDate.innerText = dateFormat(register.start)
-    listItem.appendChild(activityRegisterDate)
+function createRegisterNode(registers, registerData) {
+    const regItem = document.createElement('li')
 
-    if (register.end == null) {
-        const activityStopRegister = document.createElement('button')
-        activityStopRegister.classList.add('activity-stop-register')
-        activityStopRegister.innerText = "Stop"
-        listItem.appendChild(activityStopRegister)
+    const regTime = document.createElement('span')
+    regTime.classList.add('activity-register-time')
+    regItem.appendChild(regTime)
 
-        activityStopRegister.onclick = () => {
-            listItem.removeChild(activityStopRegister)
-            register.end = Date.now()
+    const regDate = document.createElement('span')
+    regDate.classList.add('activity-register-date')
+    regDate.innerText = dateFormat(registerData.start)
+    regItem.appendChild(regDate)
+
+    if (registerData.end == null) {
+        const regStop = document.createElement('button')
+        regStop.classList.add('activity-stop-register')
+        regStop.innerText = "Stop"
+        regItem.appendChild(regStop)
+
+        regStop.onclick = () => {
+            regItem.removeChild(regStop)
+            registerData.end = Date.now()
             save(data)
         }
 
         updateHandlers.push(() => {
-            activityRegisterTime.innerText = timeFormat(Date.now() - register.start)
-            activityRegisterTime.setAttribute('data-content', fullTimeFormat(Date.now() - register.start))
-            return register.end != null
+            const duration = registersDuration(registerData)
+            regTime.innerText = timeFormat(duration)
+            regTime.setAttribute('data-content', fullTimeFormat(duration))
+            return registerData.end != null
         })
     } else {
-        activityRegisterTime.innerText = timeFormat(register.end - register.start)
-        activityRegisterTime.setAttribute('data-content', fullTimeFormat(register.end - register.start))
+        const duration = registersDuration(registerData)
+        regTime.innerText = timeFormat(duration)
+        regTime.setAttribute('data-content', fullTimeFormat(duration))
     }
 
-    const activityDeleteRegister = document.createElement('button')
-    activityDeleteRegister.classList.add('activity-delete-register')
-    activityDeleteRegister.innerText = "×"
-    listItem.appendChild(activityDeleteRegister)
+    const regDelete = document.createElement('button')
+    regDelete.classList.add('activity-delete-register')
+    regDelete.innerText = "×"
+    regItem.appendChild(regDelete)
 
     var clicks = 3
-    activityDeleteRegister.onclick = () => {
+    regDelete.onclick = () => {
         clicks--;
-        activityDeleteRegister.innerText = clicks
+        regDelete.innerText = clicks
 
         if (clicks <= 0) {
-            registers.splice(registers.indexOf(register), 1)
-            listItem.parentNode.removeChild(listItem)
+            registers.splice(registers.indexOf(registerData), 1)
+            regItem.parentNode.removeChild(regItem)
             save(data);
         }
         setTimeout(() => {
             clicks = 3
-            activityDeleteRegister.innerText = '×'
+            regDelete.innerText = '×'
         }, 800)
     }
 
-    return listItem
+    return regItem
+}
+
+function randomId() {
+    var id = ''
+    
+    for (let i = 0; i < 10; i++) {
+        id += String(parseInt(Math.random() * 9999).toString(16)).padStart('0', 4)
+    }
+    
+    return id
+}
+
+function registersDuration(registerData) {
+    if (registerData.end != null) {
+        return registerData.end - registerData.start
+    }
+
+    return Date.now() - registerData.start
 }
 
 function dateFormat(timestamp) {
     const date = new Date(timestamp)
-    return String(date.getDate()).padStart(2, '0') + '/' +
-        String(date.getMonth() + 1).padStart(2, '0') + '/' +
-        String(date.getFullYear()).padStart(4, '0')
+
+    const day = String(date.getDate()).padStart(2, '0')
+    const mounth = String(date.getMonth() + 1).padStart(2, '0')
+    const year = String(date.getFullYear()).padStart(4, '0')
+
+    return day + '/' + mounth + '/' + year
 }
 
 function timeFormat(time) {
@@ -307,10 +346,3 @@ function fullTimeFormat(time) {
 
     return str.trim()
 }
-
-
-
-
-/*
-"{\"activities\":[{\"title\":\"pointer\",\"registers\":[{\"start\":1684419684305,\"end\":1684426833696},{\"start\":1684429543050,\"end\":1684432122601},{\"start\":1684439925822,\"end\":1684446590865},{\"start\":1684491937996,\"end\":1684493024860},{\"start\":1684494230289,\"end\":null}]},{\"title\":\"Dormir\",\"registers\":[]},{\"title\":\"Tomar café \",\"registers\":[{\"start\":1684493020430,\"end\":1684494073768}]},{\"title\":\"Aula\",\"registers\":[{\"start\":1684426832124,\"end\":1684446595894}]},{\"title\":\"Teste\",\"registers\":[{\"start\":1684501845456,\"end\":null}]},{\"title\":\"teste\",\"registers\":[]}]}"
-*/
